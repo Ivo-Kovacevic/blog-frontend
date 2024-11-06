f31f7884a589eafb8a49e3be658827aa1df3f7b9
Ivo <131997439+Ivo-Kovacevic@users.noreply.github.com>
2024-11-06 13:21:33 +0100

    change green error color to red

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,7 +1,7 @@
 export default function Error({ resource }) {
   return (
     <>
-      <h1 className="text-center text-xl text-green-500">Error getting {resource}.</h1>
+      <h1 className="text-center text-xl text-red-500">Error getting {resource}.</h1>
     </>
   );
 }
